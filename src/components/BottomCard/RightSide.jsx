7aa29e6b8c716a1d27b81f37141f8c730f6f8a31
Carlo <parachutes1994@gmail.com>
2023-01-04 23:57:43 +0100

    Fix style

--- src/components/BottomCard/RightSide.jsx
+++ src/components/BottomCard/RightSide.jsx
@@ -5,11 +5,11 @@ const RightSide = () => {
     <div className={styles.testo3}>
       <h3 className={styles.title}>Why Us</h3>
       <p className={styles.list}> Tutorials by industry experts</p>
-      <p className={styles.list}> Tutorials by industry experts</p>
-      <p className={styles.list}> Tutorials by industry experts</p>
-      <p className={styles.list}> Tutorials by industry experts</p>
-      <p className={styles.list}> Tutorials by industry experts</p>
-      <p className={styles.list}> Tutorials by industry experts</p>
+      <p className={styles.list}> Peer & expert code review</p>
+      <p className={styles.list}> Coding exercises</p>
+      <p className={styles.list}> Access to our GitHub repos</p>
+      <p className={styles.list}> Community forum</p>
+      <p className={styles.list}> New videos every week</p>
     </div>
   );
 };
